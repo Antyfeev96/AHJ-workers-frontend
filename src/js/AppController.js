556abe409c4607d544e0ff0648141dad7e055a13
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-04-20 13:29:31 +0300

    service worker url changed

--- src/js/AppController.js
+++ src/js/AppController.js
@@ -48,7 +48,7 @@ export default class AppController {
       window.addEventListener('load', async () => {
         try {
           await navigator.serviceWorker.register(
-            '/service.worker.js', { scope: './' },
+            '/AHJ-workers-frontend/service.worker.js', { scope: './' },
           );
         } catch (e) {
           console.log(e);
